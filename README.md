b06308ccd1e94f95b34a8bf742e083b8abd40900
Leibundgut <Andrew.Leibundgut@associatedbank.com>
2021-10-21 14:28:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# github-next-version
+Gets next version of project based on previous commit tag and commit messages.
+
+# Get previous tag
+
+Github Action that gets the latest tag from git
+
+![Example output showing this action in action](images/output.png)
+
+## Output
+
+This action has two output: 
+    `tag` for the latest tag this action finds.
+    `version` the next version that should be used. 
+
+The next version will be determined by commit messages since the last tag created.
+
+Commit message conventions:
+* fix: will update the 3 (patch) number of the version
+* feat: will update the 2 (minor) number of the version
+* BREAKING will update the 1(major) number of the version
+
+It should always return a `version`, when there is at least one commit.
+`tag` will only be set if there was a previous tag.
+
+## Example
+
+```yaml
+name: Generate
+jobs:
+  generate:
+    steps:
+      - uses: actions/checkout@v2
+      - name: 'Get Next tag'
+        id: semver
+        uses: "cdotyone/github-next-version@main"
+        env:
+          GITHUB_TOKEN: "${{ github.token }}"
+```
+
---
+++ index.js
@@ -0,0 +1,96 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const core = require('@actions/core');
+
+let version_file = core.getInput('version_file') || 'package.json';
+
+const processMessage = function(message,tag) {
+    let parts = tag.replace(/v/,'').trim().split('.');
+    parts.length = 3;
+
+    if (message.indexOf("fix:") > 0) {
+        parts[2] = parseInt(parts[2]);
+        parts[2]++;
+    }
+    if (message.indexOf("feat:") > 0) {
+        parts[1] = parseInt(parts[1]);
+        parts[1]++;
+        parts[2] = 0;
+    }
+    if (message.indexOf("BREAKING") > 0) {
+        parts[0] = parseInt(parts[0]);
+        parts[0]++;
+        parts[1] = '0';
+        parts[2] = '0';
+    }
+
+    tag = parts.join('.');
+    console.log('\x1b[32m%s\x1b[0m', `Next version: ${tag}`);
+    console.log(`::set-output name=version::${tag}`);
+};
+
+exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
+    if (err) {
+        console.log('\x1b[33m%s\x1b[0m', 'Could not find any revisions because: ');
+        console.log('\x1b[31m%s\x1b[0m', stderr);
+        console.log('::set-output name=version::v1.0.0');
+        process.exit(0);
+        return;
+    }
+    if(rev && rev.length>0) {
+        console.log('git describe --tags ' + rev);
+        exec(`git describe --tags ${rev}`, (err, tag, stderr) => {
+            if (err) {
+                console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
+                console.log('\x1b[31m%s\x1b[0m', stderr);
+                console.log('::set-output name=version::v0.0.1');
+                process.exit(0);
+                return;
+            }
+
+            tag = tag.trim();
+            console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+            console.log(`::set-output name=tag::${tag}`);
+
+            console.log(`git log ${tag}..HEAD --oneline`);
+            exec(`git log ${tag}..HEAD --oneline`, (err, message, stderr) => {
+                if (err) {
+                    console.log('\x1b[33m%s\x1b[0m', 'Could not find last commit message because: ');
+                    console.log('\x1b[31m%s\x1b[0m', stderr);
+                    process.exit(1);
+                    return;
+                }
+
+                console.log('\x1b[32m%s\x1b[0m', `Found message: ${message}`);
+                processMessage(message, tag);
+            });
+        });
+    } else {
+        console.log('\x1b[32m%s\x1b[0m', 'Tag not found trying config file');
+        exec("git show -s --format='%s'", (err, commit_message, stderr) => {
+            if (err) {
+                console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
+                console.log('\x1b[31m%s\x1b[0m', stderr);
+                console.log('::set-output name=version::v0.0.1');
+                process.exit(0);
+                return;
+            }
+
+            try {
+                version_file = process.cwd() + '/' + version_file;
+                console.log('\x1b[32m%s\x1b[0m', `reading config: ${version_file}`);
+                fs.readFile(version_file, 'utf8', (err, data) => {
+                    let tag = JSON.parse(data).version;
+                    tag = tag.trim();
+                    console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+                    console.log(`::set-output name=tag::${tag}`);
+                    processMessage(commit_message, tag);
+                });
+            } catch {
+                console.log('\x1b[33m%s\x1b[0m', 'Could not find last or next version: ');
+                console.log(`::set-output name=tag::v0.0.0`);
+                console.log(`::set-output name=version::v0.0.1`);
+            }
+        });
+    }
+});
